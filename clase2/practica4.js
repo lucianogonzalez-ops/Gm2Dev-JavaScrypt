7ca9fb454a13b05565cd99eb06a0c445d8e8adb8
Luciano <Luciano.gonzalez@gm2.com>
2026-06-23 12:53:47 -0300

    Carpeta de clase 2

---
+++ clase2/practica4.js
@@ -0,0 +1,7 @@
+// El usuario escribe su nombre en un formulario
+const nombreUsuario = "  "; // espacios en blanco
+if (nombreUsuario == "") {
+  console.log("El nombre no puede estar vacío");
+} else {
+  console.log("Bienvenido, " + nombreUsuario);
+}
