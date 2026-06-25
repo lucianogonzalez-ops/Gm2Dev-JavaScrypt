a40b35b47d95a2632b921c1b3adfaeb1e597a216
Luciano <Luciano.gonzalez@gm2.com>
2026-06-25 10:11:35 -0300

    Parte b de la practica 3 clase 5

--- clase5-Functions/practica3.js
+++ clase5-Functions/practica3.js
@@ -11,3 +11,14 @@ console.log(saludar("Ana","es"));          // "Hola, Ana"
 console.log(saludar("Ana","en"));          // "Hola, Ana"
 
 
+//PARTE C
+
+
+
+function precioFinal(precio,descuento = 0){
+    return precio*(100-descuento)/100
+}
+
+console.log(precioFinal(1000));       // 1000
+console.log(precioFinal(1000, 10));   // 900
+console.log(precioFinal(1000, 25));   // 750
