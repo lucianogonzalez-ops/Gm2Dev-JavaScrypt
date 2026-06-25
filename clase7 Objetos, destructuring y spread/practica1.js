ff4f1e687201292c357b7e3bbd65bfaaabc28a7f
Luciano <Luciano.gonzalez@gm2.com>
2026-06-25 11:45:54 -0300

    Practica 1 Clase 7

---
+++ clase7 Objetos, destructuring y spread/practica1.js
@@ -0,0 +1,11 @@
+const jugador = {
+  nombre: "Messi",
+  goles: 8,
+  pais: "Argentina",
+};
+console.log(jugador.nombre,jugador["pais"])
+
+jugador.goles= 9 
+jugador.club="inter de miami "
+
+console.log(jugador)
