1851df23f1dea7d40a30c6b9695da7776cb4de53
Luciano <Luciano.gonzalez@gm2.com>
2026-07-01 12:44:09 -0300

    Clase 1 Todos los ejercicios

---
+++ clase01/04-TablaDelMundial/practica.js
@@ -0,0 +1,15 @@
+console.table([
+  { equipo: "Argentina", titulos: 3, mundialjugado: 22},
+  { equipo: "Brasil", titulos: 5 , mundialjugado: 22},
+  { equipo: "Francia", titulos: 2 , mundialjugado: 15},
+]);
+
+//¿Qué diferencia notás con un console.log normal de lo mismo? Probá las dos y compará.
+console.log([
+  { equipo: "Argentina", titulos: 3, mundialjugado: 22},
+  { equipo: "Brasil", titulos: 5 , mundialjugado: 22},
+  { equipo: "Francia", titulos: 2 , mundialjugado: 15},
+]);
+
+//la tabla es más visual y ordenada, mientras que el log normal es más desordenado y menos legible.
+
