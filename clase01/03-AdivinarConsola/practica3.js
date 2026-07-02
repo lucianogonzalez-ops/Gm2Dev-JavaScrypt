b03bec7e353d8ccfd2997ba6019f489c3e497aec
Luciano <Luciano.gonzalez@gm2.com>
2026-07-02 10:59:51 -0300

    Ejercicios 1-3 Clase 1

---
+++ clase01/03-AdivinarConsola/practica3.js
@@ -0,0 +1,10 @@
+console.log(5 * 3); // da 15
+console.log("5" * 3);// da 15, ya que el asterisco lo toma como numero
+console.log("5" + 3);// no da 8, ya que el mas los suma como string
+console.log(7 % 2);// da 1, ya que el porcentaje es el resto de la division
+console.log("Gol" + "azo");// da Golazo, ya que el mas los suma como string
+console.log("3" - 1);// da 2, ya que el menos lo toma como numero
+
+//console.log(Hola mundo);  da error, ya que no esta entre comillas
+
+console.log("Hola mundo"); // da Hola mundo, ya que esta entre comillas
